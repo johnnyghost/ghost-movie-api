3dcb5e4d6934af63ac37537bea6c62baffcb67fa
johnnyghost <kemarx@gmail.com>
2015-08-11 15:11:58 +0100

    Update gitignore.

--- app/core/config/clients.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    tmdb: {
-        baseUrl: 'api.themoviedb.org',
-        version: 3,
-        apiKey: 'a40440b052edf354fe17b0fea6da9540'
-    }
-};
--- app/core/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    clients: require('./clients'),
-    logger: require('./logger')
-};
