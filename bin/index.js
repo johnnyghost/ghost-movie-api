2ed7c43ecd24221a6b7f61e5dd92899810d52b12
johnnyghost <kemarx@gmail.com>
2015-08-12 17:53:07 +0100

    [fix] environment logic.

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app     = express();
 
-// load environment configuration
-// TODO: in dev only
-require('./environment');
+
+if ('development' == app.get('env')) {
+    // load environment configuration
+    // TODO: in dev only
+    require('./environment');
+}
+
 
 // routes
 require('./../app/core/routes')(app);
