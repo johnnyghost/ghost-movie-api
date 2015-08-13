dfdd4ca537284f07aceabdc7fba538847fbed6c6
johnnyghost <kemarx@gmail.com>
2015-08-13 12:33:48 +0100

    update mongodb

--- app/core/clients/mongoDbClient.js
+++ app/core/clients/mongoDbClient.js
@@ -1,13 +1,6 @@
 var mongoose = require ('mongoose');
 var mongoConfig = require('./../config').clients.mongodb;
 
-mongoose.connect('mongodb://' + mongoConfig.dbUser + ':' + mongoConfig.dbPassword + '@ds033113.mongolab.com:33113/heroku_jn8zpdrz', function (err, res) {
-    console.log(err);
-    if (err) {
-        console.log(1);
-    } else {
-        console.log(2);
-    }
-});
+mongoose.connect(mongoConfig.uri);
 
 module.exports = mongoose;
--- app/core/config/clients.js
+++ app/core/config/clients.js
@@ -5,8 +5,7 @@ module.exports = {
         apiKey: process.env.TMDB_API_KEY
     },
     mongodb: {
-        uri: 'ds033113.mongolab.com:33113/heroku_jn8zpdrz',
-        dbUser: process.env.MONGO_DB_USER,
-        dbPassword: process.env.MONGO_DB_PASSWORD
+        uri: 'mongodb://' + process.env.MONGO_DB_USER + ':' +
+            process.env.MONGO_DB_PASSWORD + '@ds033113.mongolab.com:33113/heroku_jn8zpdrz'
     }
 };
